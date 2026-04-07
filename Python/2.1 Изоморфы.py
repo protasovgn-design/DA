956ac8ff3f4ba5cc7608d0620893110b7fe2e12c
protasovgn-design <protasov_gn@elamed.com>
2026-04-07 11:25:12 +0300

    Create 2.1 Изоморфы.py

---
+++ Python/2.1 Изоморфы.py
@@ -0,0 +1,16 @@
+def is_isomorphic(s, t): if len(s) != len(t): return False
+
+map_s_to_t = {}
+map_t_to_s = {}
+
+for char_s, char_t in zip(s,t):
+    if char_s in map_s_to_t and map_s_to_t[char_s] != char_t:
+        return False
+
+    if char_t in map_t_to_s and map_t_to_s[char_t] != char_s:
+        return False
+
+    map_s_to_t[char_s] = char_t
+    map_t_to_s[char_t] = char_s
+
+return True
